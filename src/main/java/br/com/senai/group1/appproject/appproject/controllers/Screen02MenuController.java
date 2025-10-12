7b2a25b29b56668ce991f97f4b17e21c24a129b6
Guilherme Pereira <guilhermepereiraprofessor@gmail.com>
2025-10-12 00:36:26 -0300

    Adicionando método para carregar cena 03

--- src/main/java/br/com/senai/group1/appproject/appproject/controllers/Screen02MenuController.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/controllers/Screen02MenuController.java
@@ -1,11 +1,14 @@
 package br.com.senai.group1.appproject.appproject.controllers;
 
+import br.com.senai.group1.appproject.appproject.models.SceneHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -37,11 +40,23 @@ public class Screen02MenuController implements Initializable {
         this.buttonClose.setOnAction((ActionEvent event) -> {
            this.buttonCloseEvent();
         });
+        this.buttonSettings.setOnAction((ActionEvent event) -> {
+           this.buttonSettingsEvent();
+        });
     }
 
     public void buttonPlayEvent() {}
-    public void buttonSettingsEvent() {}
+
+    public void buttonSettingsEvent() {
+        Scene scene = this.buttonSettings.getScene();
+        Stage stage = (Stage) scene.getWindow();
+
+        stage.setScene(SceneHandler.screen03Settings);
+    }
+
     public void buttonCreditsEvent() {}
+
+
     public void buttonCloseEvent() {
         System.exit(0);
     }
