8fde9a9351bb5331201fb0eee0279340f9d9ca19
Guilherme Pereira <guilhermepereiraprofessor@gmail.com>
2025-10-12 00:59:16 -0300

    Desenvolvimento da cena 08 - Créditos

--- src/main/java/br/com/senai/group1/appproject/appproject/controllers/Screen08CreditsController.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/controllers/Screen08CreditsController.java
@@ -1,29 +1,42 @@
 package br.com.senai.group1.appproject.appproject.controllers;
 
+import br.com.senai.group1.appproject.appproject.models.SceneHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Screen08CreditsController implements Initializable {
+    @FXML
+    private ImageView backgroundImageView;
 
+    @FXML
+    private Button buttonBack;
+
+    @FXML
+    private StackPane container;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.buttonBack.setOnAction((ActionEvent event) -> {
+            this.buttonBackEvent();
+        });
 
 
     }
 
-    public void buttonPlayEvent() {}
-    public void buttonSettingsEvent() {}
-    public void buttonCreditsEvent() {}
-    public void buttonCloseEvent() {
-        System.exit(0);
+    public void buttonBackEvent() {
+        Scene scene = this.buttonBack.getScene();
+        Stage stage = (Stage) scene.getWindow();
+
+        stage.setScene(SceneHandler.screen02Menu);
     }
 
 }
