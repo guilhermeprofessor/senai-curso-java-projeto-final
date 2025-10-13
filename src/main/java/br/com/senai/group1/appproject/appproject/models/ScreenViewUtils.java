437a3e7e38934ca3afc48a58f407349c8422a80a
Guilherme Pereira <guilhermepereiraprofessor@gmail.com>
2025-10-13 14:30:15 -0300

    Desenvolvendo classe para facilitar a obtenção das dimensões da tela

--- src/main/java/br/com/senai/group1/appproject/appproject/models/ScreenViewUtils.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/models/ScreenViewUtils.java
@@ -1,5 +1,8 @@
 package br.com.senai.group1.appproject.appproject.models;
 
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
+
 public class ScreenViewUtils {
 
     public static final String SCREEN_01_OPENING_FXML = "/views/screen-01-opening.fxml";
@@ -10,4 +13,39 @@ public class ScreenViewUtils {
     public static final String SCREEN_06_DIALOGS_FXML = "/views/screen-06-dialogs.fxml";
     public static final String SCREEN_07_FIGHT_FXML = "/views/screen-07-fight.fxml";
     public static final String SCREEN_08_CREDITS_FXML = "/views/screen-08-credits.fxml";
+
+    private static double screenWidth = 0;
+    private static double screenHeight = 0;
+    private static double minDimension = 400;
+
+    public static Rectangle2D getThePositionToCenterTheScreen() {
+        Screen screen = Screen.getPrimary();
+        Rectangle2D boundValues = screen.getBounds();
+
+        Rectangle2D newPosition = new Rectangle2D(
+            0,
+            0,
+            (boundValues.getWidth() / 2) - ScreenViewUtils.getScreenWidth() / 2,
+            (boundValues.getHeight() / 2) - ScreenViewUtils.getScreenHeight() / 2
+        );
+
+        return newPosition;
+    }
+
+
+    public static double getScreenWidth() {
+        return screenWidth;
+    }
+
+    public static void setScreenWidth(double value) {
+        ScreenViewUtils.screenWidth = value > ScreenViewUtils.minDimension ? value : ScreenViewUtils.minDimension;
+    }
+
+    public static double getScreenHeight() {
+        return screenHeight;
+    }
+
+    public static void setScreenHeight(double value) {
+        ScreenViewUtils.screenHeight = value > ScreenViewUtils.minDimension ? value : ScreenViewUtils.minDimension;
+    }
 }
