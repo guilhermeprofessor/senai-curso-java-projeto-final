9f6874a9f0dace735f2212aa074504e591b1bdcc
Guilherme Pereira <guilhermepereiraprofessor@gmail.com>
2025-10-13 14:29:20 -0300

    Setando dimensão padrão das telas e colocando janela no centro da tela

--- src/main/java/br/com/senai/group1/appproject/appproject/MainApplication.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/MainApplication.java
@@ -6,12 +6,15 @@ import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class MainApplication extends Application {
@@ -19,16 +22,25 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
 //        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_01_OPENING_FXML));
 //        String classe = getClass();
-        Scene scene = SceneHandler.loadScreen01OpeningScene(stage);
-        stage.setScene(scene);
 
-        SceneHandler.screen01Opening = scene;
+        ScreenViewUtils.setScreenWidth(1280);
+        ScreenViewUtils.setScreenHeight(720);
+
+        SceneHandler.screen01Opening = SceneHandler.loadScreen01OpeningScene(stage);
         SceneHandler.screen02Menu = SceneHandler.loadScreen02MenuScene(stage);
         SceneHandler.screen03Settings = SceneHandler.loadScreen03SettingsScene(stage);
         SceneHandler.screen04Tutorial = SceneHandler.loadScreen04TutorialScene(stage);
         SceneHandler.screen05SelectPlayer = SceneHandler.loadScreen05SelectPlayerScene(stage);
         SceneHandler.screen07Fight = SceneHandler.loadScreen07FightScene(stage);
         SceneHandler.screen08Credits = SceneHandler.loadScreen08CreditsScene(stage);
+
+
+        stage.setScene(SceneHandler.screen01Opening);
+
+        Rectangle2D screenCenterValues = ScreenViewUtils.getThePositionToCenterTheScreen();
+        stage.setX(screenCenterValues.getWidth());
+        stage.setY(screenCenterValues.getHeight());
+
 
 
         stage.initStyle(StageStyle.TRANSPARENT);
