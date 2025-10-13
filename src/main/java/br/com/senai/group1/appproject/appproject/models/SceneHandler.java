2904664502024617d5a58bc41a015f992e0da6e7
Guilherme Pereira <guilhermepereiraprofessor@gmail.com>
2025-10-12 23:35:29 -0300

    Corrigindo nome de variável e arquivo fxml

--- src/main/java/br/com/senai/group1/appproject/appproject/models/SceneHandler.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/models/SceneHandler.java
@@ -8,7 +8,7 @@ public class SceneHandler {
     public static Scene screen02Menu = null;
     public static Scene screen03Settings = null;
     public static Scene screen04Tutorial = null;
-    public static Scene screen05Select_player = null;
+    public static Scene screen05SelectPlayer = null;
     public static Scene screen06Dialogs = null;
     public static Scene screen07Fight = null;
     public static Scene screen08Credits = null;
--- src/main/java/br/com/senai/group1/appproject/appproject/models/ScreenViewUtils.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/models/ScreenViewUtils.java
@@ -6,7 +6,7 @@ public class ScreenViewUtils {
     public static final String SCREEN_02_MENU_FXML = "/views/screen-02-menu.fxml";
     public static final String SCREEN_03_SETTINGS_FXML = "/views/screen-03-settings.fxml";
     public static final String SCREEN_04_TUTORIAL_FXML = "/views/screen-04-tutorial.fxml";
-    public static final String SCREEN_05_SELECT_PLAYER_FXML = "/views/screen-05-select_player.fxml";
+    public static final String SCREEN_05_SELECT_PLAYER_FXML = "/views/screen-05-select-player.fxml";
     public static final String SCREEN_06_DIALOGS_FXML = "/views/screen-06-dialogs.fxml";
     public static final String SCREEN_07_FIGHT_FXML = "/views/screen-07-fight.fxml";
     public static final String SCREEN_08_CREDITS_FXML = "/views/screen-08-credits.fxml";
