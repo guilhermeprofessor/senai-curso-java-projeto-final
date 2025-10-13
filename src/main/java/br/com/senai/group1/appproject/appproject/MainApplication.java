fc07e808b259da7b4df0052942ecdaf81fb66897
Guilherme Pereira <guilhermepereiraprofessor@gmail.com>
2025-10-13 00:08:26 -0300

    Movendo métodos para a classe Scenehandler para torná-los acessíveis de qualquer ponto do sistema

--- src/main/java/br/com/senai/group1/appproject/appproject/MainApplication.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/MainApplication.java
@@ -18,16 +18,17 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_01_OPENING_FXML));
-        Scene scene = this.loadScreen01OpeningScene(stage);
+//        String classe = getClass();
+        Scene scene = SceneHandler.loadScreen01OpeningScene(stage);
         stage.setScene(scene);
 
         SceneHandler.screen01Opening = scene;
-        SceneHandler.screen02Menu = this.loadScreen02MenuScene(stage);
-        SceneHandler.screen03Settings = this.loadScreen03SettingsScene(stage);
-        SceneHandler.screen04Tutorial = this.loadScreen04TutorialScene(stage);
-        SceneHandler.screen05SelectPlayer = this.loadScreen05SelectPlayerScene(stage);
-        SceneHandler.screen07Fight = this.loadScreen07FightScene(stage);
-        SceneHandler.screen08Credits = this.loadScreen08CreditsScene(stage);
+        SceneHandler.screen02Menu = SceneHandler.loadScreen02MenuScene(stage);
+        SceneHandler.screen03Settings = SceneHandler.loadScreen03SettingsScene(stage);
+        SceneHandler.screen04Tutorial = SceneHandler.loadScreen04TutorialScene(stage);
+        SceneHandler.screen05SelectPlayer = SceneHandler.loadScreen05SelectPlayerScene(stage);
+        SceneHandler.screen07Fight = SceneHandler.loadScreen07FightScene(stage);
+        SceneHandler.screen08Credits = SceneHandler.loadScreen08CreditsScene(stage);
 
 
         stage.initStyle(StageStyle.TRANSPARENT);
@@ -55,51 +56,4 @@ public class MainApplication extends Application {
         launch();
     }
 
-    public Scene loadScreen01OpeningScene(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_01_OPENING_FXML));
-        Scene scene = new Scene(root, 1280, 720);
-
-        return scene;
-    }
-
-    public Scene loadScreen02MenuScene(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_02_MENU_FXML));
-        Scene scene = new Scene(root, 1280, 720);
-
-        return scene;
-    }
-
-    public Scene loadScreen03SettingsScene(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_03_SETTINGS_FXML));
-        Scene scene = new Scene(root, 1280, 720);
-
-        return scene;
-    }
-
-    public Scene loadScreen04TutorialScene(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_04_TUTORIAL_FXML));
-        Scene scene = new Scene(root, 1280, 720);
-
-        return scene;
-    }
-
-    public Scene loadScreen05SelectPlayerScene(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_05_SELECT_PLAYER_FXML));
-        Scene scene = new Scene(root, 1280, 720);
-
-        return scene;
-    }
-
-    public Scene loadScreen07FightScene(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_07_FIGHT_FXML));
-        Scene scene = new Scene(root, 1280, 720);
-
-        return scene;
-    }
-    public Scene loadScreen08CreditsScene(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(ScreenViewUtils.SCREEN_08_CREDITS_FXML));
-        Scene scene = new Scene(root, 1280, 720);
-
-        return scene;
-    }
 }
--- src/main/java/br/com/senai/group1/appproject/appproject/models/SceneHandler.java
+++ src/main/java/br/com/senai/group1/appproject/appproject/models/SceneHandler.java
@@ -1,6 +1,12 @@
 package br.com.senai.group1.appproject.appproject.models;
 
+import br.com.senai.group1.appproject.appproject.MainApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SceneHandler {
 
@@ -12,4 +18,53 @@ public class SceneHandler {
     public static Scene screen06Dialogs = null;
     public static Scene screen07Fight = null;
     public static Scene screen08Credits = null;
+
+
+    public static Scene loadScreen01OpeningScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource(ScreenViewUtils.SCREEN_01_OPENING_FXML));
+        Scene scene = new Scene(root, 1280, 720);
+
+        return scene;
+    }
+
+    public static Scene loadScreen02MenuScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource(ScreenViewUtils.SCREEN_02_MENU_FXML));
+        Scene scene = new Scene(root, 1280, 720);
+
+        return scene;
+    }
+
+    public static Scene loadScreen03SettingsScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource(ScreenViewUtils.SCREEN_03_SETTINGS_FXML));
+        Scene scene = new Scene(root, 1280, 720);
+
+        return scene;
+    }
+
+    public static Scene loadScreen04TutorialScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource(ScreenViewUtils.SCREEN_04_TUTORIAL_FXML));
+        Scene scene = new Scene(root, 1280, 720);
+
+        return scene;
+    }
+
+    public static Scene loadScreen05SelectPlayerScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource(ScreenViewUtils.SCREEN_05_SELECT_PLAYER_FXML));
+        Scene scene = new Scene(root, 1280, 720);
+
+        return scene;
+    }
+
+    public static Scene loadScreen07FightScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource(ScreenViewUtils.SCREEN_07_FIGHT_FXML));
+        Scene scene = new Scene(root, 1280, 720);
+
+        return scene;
+    }
+    public static Scene loadScreen08CreditsScene(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(MainApplication.class.getResource(ScreenViewUtils.SCREEN_08_CREDITS_FXML));
+        Scene scene = new Scene(root, 1280, 720);
+
+        return scene;
+    }
 }
