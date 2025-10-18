496447d100b3bb27da20c38e1ee88c6a54f61050
Guilherme Pereira <guilhermepereiraprofessor@gmail.com>
2025-10-18 02:25:02 -0300

    Desenvolvendo classe de estados

---
+++ src/main/java/br/com/senai/group1/appproject/appproject/models/AnimationStateEnum.java
@@ -0,0 +1,11 @@
+package br.com.senai.group1.appproject.appproject.models;
+
+public enum AnimationStateEnum {
+    ATTACK,
+    DAMAGE,
+    DEFENSE,
+    DODGE,
+    ENERGY,
+    IDLE,
+    SPECIAL_POWER
+}
